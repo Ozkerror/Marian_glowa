25ffbc49194bb44e4350e294570c104021efac8c
LemonDem0n <hi.gwarek@gmail.com>
2025-03-20 11:54:23 +0100

    Update main.py

--- PythonProject/main.py
+++ PythonProject/main.py
@@ -34,37 +34,41 @@ g_wspolczynnik_lp = 1
 g_wspolczynnik_gd = 1
 odliczanie = 0
 
-arduino = serial.Serial('COM3', 9600)
+arduino = serial.Serial('COM3', 9600) 
 time.sleep(2)  # zeby sie polaczenie ustabilizowalo
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml') # Zaladowanie klasyfikatora, model do wykrywania twarzy
 nagranie = cv2.VideoCapture(0)
 
 if not nagranie.isOpened():
     print("nie udalo sie otworzyc kamery")
     exit()
 
-sz_kamery = int(nagranie.get(cv2.CAP_PROP_FRAME_WIDTH))
-w_kamery = int(nagranie.get(cv2.CAP_PROP_FRAME_HEIGHT))
+sz_kamery = int(nagranie.get(cv2.CAP_PROP_FRAME_WIDTH)) # Pobranie szerokosci klatki wideo w pikselach
+w_kamery = int(nagranie.get(cv2.CAP_PROP_FRAME_HEIGHT)) # Pobranie wysokosci klatki wideo w pikselach
 
 funkcje.centrowanie(arduino, serwo_lp, serwo_kat_lp, serwo_gd, serwo_kat_gd, serwo_oko_lp, serwo_oko_gd,
-                    serwo_kat_oko_lp, serwo_kat_oko_gd)
+                    serwo_kat_oko_lp, serwo_kat_oko_gd) 
 
-while True:
+while True:                                     # Odczytywanie klatek nagrania, jesli sprawdzenie jest false to oznacza koniec nagrania
     sprawdzenie, klatka = nagranie.read()
     if not sprawdzenie:
         break
 
-    szara_klatka = cv2.cvtColor(klatka, cv2.COLOR_BGR2GRAY)
-    twarz = face_cascade.detectMultiScale(szara_klatka, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    szara_klatka = cv2.cvtColor(klatka, cv2.COLOR_BGR2GRAY) # Konwersja obrazu na skale szarosci, klatka - obraz RGB, szara_klatka - nowy obraz w odcienach szarosci
+    twarz = face_cascade.detectMultiScale(szara_klatka, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30)) # Wykrywanie twarzy, skalowanie obrazu,  
+                                                                                                           # minNeighbors - Liczba sąsiadujących prostokątów (kandydatów na twarz), które muszą zostać wykryte, aby uznać, że jest tam faktyczna twarz,
+                                                                                                           # minSize=(30, 30) - Minimalny rozmiar wykrywanej twarzy w pikselach.
 
     for (x, y, sz_twarzy, wys_twarzy) in twarz:
-        cv2.rectangle(klatka, (x, y), (x + sz_twarzy, y + wys_twarzy), (100, 100, 100), 3)
+        cv2.rectangle(klatka, (x, y), (x + sz_twarzy, y + wys_twarzy), (100, 100, 100), 3) # Kod przechodzi przez wszystkie wykryte twarze i rysuje wokol nich prostokat
 
         srodek_x = funkcje.srodek(x, sz_twarzy)
         srodek_y = funkcje.srodek(y, wys_twarzy)
         prop_x = funkcje.pozycja_x(x, sz_kamery, sz_twarzy)
         prop_y = funkcje.pozycja_y(y, w_kamery, wys_twarzy)
         funkcje.ruch_oczu(o_wspolczynnik_lp, o_wspolczynnik_gd, prop_x, prop_y, arduino, serwo_oko_gd, serwo_oko_lp)
+
+        # wywolanie funkcji sprawdzania 
 
         ss = funkcje.sprawdz_ss(prop_x, prop_y)
         sg = funkcje.sprawdz_sg(prop_x, prop_y)
@@ -103,11 +107,15 @@ while True:
         elif pd:
             sektor = 9
 
+
+        # spawdzanie czy robot powinien ruszac glowa czy jest np zbyt blisko lewej krawedzi
         if skrajne_l or skrajne_g or skrajne_d or skrajne_p or (odliczanie >= czas_glowa):
             funkcje.ruch_glowy(arduino, g_wspolczynnik_lp, g_wspolczynnik_gd, sz_kamery, w_kamery, srodek_x, srodek_y,
                                serwo_lp, serwo_gd)
             odliczanie = 0
 
+
+        # sprawdzanie czy sektor sie zmienil
         if sektor != 0 and sektor != poprzedni:
             odliczanie = time.time()
 
@@ -116,8 +124,8 @@ while True:
 
         poprzedni = sektor
 
-    cv2.imshow("nagrywanie", klatka)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    cv2.imshow("nagrywanie", klatka) # wyswietla klatke w okienku nagrywanie
+    if cv2.waitKey(1) & 0xFF == ord('q'): # pozwolenie uzytkownikowi na zakonczenie dzialania programu poprzez nacisniecie klawisza 'q'
         break
 
 nagranie.release()

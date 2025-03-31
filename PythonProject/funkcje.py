12c4cca65300a6e088ca081a26edaa4f2c871a0d
Ozkerror <ozkerror@gmail.com>
2025-03-31 20:43:11 +0200

    pierwsza werjsa do testow

--- PythonProject/funkcje.py
+++ PythonProject/funkcje.py
@@ -1,9 +1,7 @@
 from enum import Enum
 import time
 
-from numpy.ma.core import minimum
-
-
+#stworzenie pomocniczego typu danych dla wiekszej przejrzystosci funkcji sprawdz.sektor
 class Sektor(Enum):
     SS=1
     SD=2
@@ -15,23 +13,59 @@ class Sektor(Enum):
     PD=8
     PG=9
 
+#funkcja ktora zatrzymuje program i wykonuje sie w kolko poki nie przeslemy przez serial oczekiwanej wiadomosci, dodatkowo sluzy troche jak debuger(sprawdza przeslane wartosci)
+def potwierdzenie(rduino, oczekiwana_wiadomosc):
+    wiadomosc=""
+    while True:
+        if rduino.in_waiting > 0: #jesli w buforze odbiorczym znajduja sie jakiekolwiek dane to warunek spelniony
+            wiadomosc = rduino.readline().decode().strip() #odczytujemy te dane, dekodujemy ciag bajtow na stringa, usuwamy biale znaki czyli spacje, /r, /n
+            print(wiadomosc) #linijka ktora w polaczeniu z programem w C pozwala na sprawdzenie czy dane zostaly przeslane poprawnie.
+        if wiadomosc == oczekiwana_wiadomosc:
+            break
+        time.sleep(0.01) #podobno pomoze uniknac niepotrzebnego obciazenia CPU
 
-#wyznaczanie wspolrzednej srodka twarzy,
+#funkcja ktora zajmuje sie przeslaniem danych do arduino
+def komunikacja_arduino(arduino, glowa_x, glowa_y, oczy_x, oczy_y, wiad_start, wiad_potwierdzajaca):
+    potwierdzenie(arduino, wiad_start) #oczekiwanie na gotowosc arduino
+    dane=[glowa_x, glowa_y, oczy_x, oczy_y]
+    arduino.write(bytearray(dane)) #przeslanie ciagu bajtow , 1 bajt to jedna pozycja serwa poniewa zawieraja sie one w zakresie od 0 do 255
+    potwierdzenie(arduino, wiad_potwierdzajaca) #oczekiwanie na potwierdzenie odbioru danych
 
+#wyznaczanie pozycji serwa ktore steruje oczami
+def ruch_oczu(wymiar_twarzy, wymiar_kamery, wspolrzedna_twarzy, wspolczynnik,minimumm, maximum):
+    proporcja=(wspolrzedna_twarzy+(wymiar_twarzy/2))/wymiar_kamery
+    pozycja=minimumm+(proporcja*(180-minimumm-maximum))
+    return int(wspolczynnik*pozycja)
 
-def pozycja_x(x, sz_kamery, sz_glowy):
+ #wyznaczanie pozycji serwa ktore steruje glowa, jest ona bardziej skomplikowana bo musi wyznaczyc najpierw o ile ma sie przesunac serwo wzgledem poprzedniej pozycji
+def ruch_glowy(wymiar_twarzy, wymiar_kamery, wspolrzedna_twarzy, wspolczynnik, poprzednia_pozycja, minimum_pozycja, maximum_pozycja):
+    odlegosc_od_srodka=wymiar_kamery/2-(wspolrzedna_twarzy+(wymiar_twarzy/2)) #odleglosc glowy od srodka, ujemna to glowa w prawej polowce, dodatnia to glowa w lewej polowec(analogicznie gora-dol)
+    proporcja=odlegosc_od_srodka/(wymiar_kamery/2) #proporcja odleglosc glowy od srodka do wymiaru polowy kamery
+    przesuniecie=wspolczynnik*proporcja*90 #wyznaczanie o ile ma sie zmienic pozycja serwa
+    if(poprzednia_pozycja+przesuniecie)<minimum_pozycja: #jesli serwo juz sie nie bedzie moglo bardziej przesunac to ustawiamy skrajna wartosc
+        return minimum_pozycja
+    elif (poprzednia_pozycja+przesuniecie)>maximum_pozycja: #analogia tylko w druga strone
+        return maximum_pozycja
+    else:
+        return int(poprzednia_pozycja+przesuniecie) #jesli wszystko git to pozycja serwa to poprzednia pozycja plus zmiana
+
+#funkcja pomocnicza wyznaczajaca stosunek srodka twarzy do calej dlugosci obrazu
+def proporcja_x(x, sz_kamery, sz_glowy):
     srodek_glowy_x = x + (sz_glowy / 2)
-    proporcja_x = srodek_glowy_x / sz_kamery
-    return proporcja_x
+    prop_x = srodek_glowy_x / sz_kamery
+    return prop_x
 
-def pozycja_y(y, w_kamery, w_glowy):
-    srodek_glowy_y = y + w_glowy / 2
-    proporcja_y = srodek_glowy_y / w_kamery
-    return proporcja_y
+#analogia
+def proporcja_y(y, w_kamery, w_glowy):
+    srodek_glowy_y = y + (w_glowy / 2)
+    prop_y = srodek_glowy_y / w_kamery
+    return prop_y
 
     #sprawdzanie czy twarz znajduje sie w konkretnych polach siatki
-def sprawdz_sektor(prop_x, prop_y):
-    if(0.2 < prop_x < 0.8 and prop_y > 0.8):
+def sprawdz_sektor(x, sz_kamery, sz_glowy, y, wys_kamery, wys_glowy):
+    prop_x=proporcja_x(x, sz_kamery, sz_glowy)
+    prop_y=proporcja_y(y, wys_kamery, wys_glowy)
+    if(0.2 < prop_x < 0.8 and 0.2 < prop_y < 0.8):
         return Sektor.SS
     if(0.2 < prop_x < 0.8 and prop_y > 0.8):
         return Sektor.SG
@@ -49,49 +83,3 @@ def sprawdz_sektor(prop_x, prop_y):
         return Sektor.PG
     if(prop_x > 0.8 and prop_y < 0.2):
         return Sektor.PD
-
-#funkcje sprawdzajace czy twarz znajduje sie w skrajnych polozeniach
-def skrajne_l(x, sz_kamery, przedzial):
-    polozenie = x / sz_kamery
-    return polozenie < przedzial
-def skrajne_p(x, sz_twarzy, sz_kamery, przedzial):
-    polozenie = (x + sz_twarzy) / sz_kamery
-    return polozenie > przedzial
-def skrajne_g(y, wys_kamery, przedzial):
-    polozenie = y / wys_kamery
-    return polozenie < przedzial
-def skrajne_d(y, wys_twarzy, wys_kamery, przedzial):
-    polozenie = (y + wys_twarzy) / wys_kamery
-    return polozenie > przedzial
-
-def potwierdzenie(rduino, oczekiwana_wiadomosc): #funkcja ktora zatrzymuje program i wykonuje sie w kolko poki nie przeslemy przez serial oczekiwanej wiadomosci
-    wiadomosc=""
-    while True:
-        if rduino.in_waiting > 0: #jesli w buforze odbiorczym znajduja sie jakiekolwiek dane to warunek spelniony
-            wiadomosc = rduino.readline().decode().strip() #odczytujemy te dane, dekodujemy ciag bajtow na stringa, usuwamy biale znaki czyli spacje, /r, /n
-            print(wiadomosc) #linijka ktora w polaczeniu z programem w C pozwala na sprawdzenie czy dane zostaly przeslane poprawnie.
-        if wiadomosc == oczekiwana_wiadomosc:
-            break
-        time.sleep(0.01) #podobno pomoze uniknac niepotrzebnego obciazenia CPU
-
-def komunikacja_arduino(arduino, glowa_lp, glowa_gd, oczy_lp, oczy_gd, wiad_start, wiad_potwierdzajaca):
-    potwierdzenie(arduino, wiad_start) #oczekiwanie na gotowosc arduino
-    dane=[glowa_lp, glowa_gd, oczy_lp, oczy_gd]
-    arduino.write(bytearray(dane)) #przeslanie ciagu bajtow , 1 bajt to jedna pozycja serwa poniewa zawieraja sie one w zakresie od 0 do 255
-    potwierdzenie(arduino, wiad_potwierdzajaca) #oczekiwanie na potwierdzenie odbioru danych
-
-def ruch_oczu(wymiar_twarzy, wymiar_kamery, wspolrzedna_twarzy, wspolczynnik,minimum, maximum,) #wyznaczanie pozycji serwa ktore steruje oczami
-    proporcja=wspolrzedna_twarzy+(wymiar_twarzy/2)/wymiar_kamery
-    pozycja=minimum+(proporcja*(180-minimum-maximum))
-    return int(wspolczynnik*pozycja)
-
-def ruch_glowy(wymiar_twarzy, wymiar_kamery, wspolrzedna_twarzy, wspolczynnik, poprzednia_pozycja, minimum_pozycja, maximum_pozycja) #wyznaczanie pozycji serwa ktore steruje glowa
-    odlegosc_od_srodka=wymiar_kamery/2-(wspolrzedna_twarzy+(wymiar_twarzy/2)) #odleglosc glowy od srodka, ujemna to glowa w prawej polowce, dodatnia to glowa w lewej polowec(analogicznie gora-dol)
-    proporcja=odlegosc_od_srodka/(wymiar_kamery/2) #proporcja odleglosc glowy od srodka do wymiaru polowy kamery
-    przesuniecie=wspolczynnik*proporcja*90 #wyznaczanie o ile ma sie zmienic pozycja serwa
-    if(poprzednia_pozycja+przesuniecie)<minimum_pozycja: #jesli serwo juz sie nie bedzie moglo bardziej przesunac to ustawiamy skrajna wartosc
-        return minimum_pozycja
-    elif (poprzednia_pozycja+przesuniecie)>maximum_pozycja: #analogia tylko w druga strone
-        return maximum_pozycja
-    else:
-        return int(poprzednia_pozycja+przesuniecie) #jesli wszystko git to pozycja serwa to poprzednia pozycja plus zmiana
--- PythonProject/main.py
+++ PythonProject/main.py
@@ -10,7 +10,7 @@ import cv2
 import serial
 import time
 import funkcje
-from funkcje import ruch_oczu, ruch_glowy, komunikacja_arduino
+from funkcje import ruch_oczu, ruch_glowy, komunikacja_arduino, Sektor
 
 port='COM3' #tutaj nalezy wpisac port do ktorego podlaczone jest arduino
 oczy_kat_lp=0
@@ -19,22 +19,35 @@ glowa_kat_lp=0
 glowa_kat_gd=0
 
 sektor = 0
-start = 0
-poprzedni = 0
 
-czas_glowa = 3 #czas w s przez ktory glowa musi przebywac w jednym z bocznych sektorow aby glowa sie ruszyla
+poprzedni_czas=0
+aktualny_czas=0
+prog_czasu = 3 #czas w s przez ktory glowa musi przebywac w jednym z bocznych sektorow aby glowa sie ruszyla
 #wspolczynniki potrzebne do odpowiedniego sterowania serwami
-o_wspolczynnik_lp = 1
-o_wspolczynnik_gd = 1
-g_wspolczynnik_lp = 1
-g_wspolczynnik_gd = 1
+o_wspolczynnik_x = 1
+o_wspolczynnik_y = 1
+g_wspolczynnik_x = 1
+g_wspolczynnik_y = 1
 odliczanie = 0
 #wartosci domyslne w ktorych marian powinien sie ustawic po wlaczeniu programu
 domyslne_x_glowy=0
 domyslne_y_glowy=0
 domyslne_x_oczu=0
 domyslne_y_oczu=0
-
+#zmienne w ktorych bedzie przechowywana pozycja serw
+pozycja_x_glowy = domyslne_x_glowy
+pozycja_y_glowy = domyslne_y_glowy
+pozycja_x_oczu = domyslne_x_oczu
+pozycja_y_oczu = domyslne_y_oczu
+#zakresy serw dla glowy i oczu
+minimum_x_glowy=0
+maximum_x_glowy=0
+minimum_y_glowy=0
+maximum_y_glowy=0
+minimum_x_oczu=0
+maximum_x_oczu=0
+minimum_y_oczu=0
+maximum_y_oczu=0
 wiadomosc_startowa="START"
 wiadomosc_potwierdzajaca="OK"
 arduino = serial.Serial(port, 9600) #tworzy obiekt z ktorym bedziemy sie komunikowac
@@ -48,7 +61,7 @@ if not nagranie.isOpened():
     exit()
 
 sz_kamery = int(nagranie.get(cv2.CAP_PROP_FRAME_WIDTH)) # Pobranie szerokosci klatki wideo w pikselach
-w_kamery = int(nagranie.get(cv2.CAP_PROP_FRAME_HEIGHT)) # Pobranie wysokosci klatki wideo w pikselach
+wys_kamery = int(nagranie.get(cv2.CAP_PROP_FRAME_HEIGHT)) # Pobranie wysokosci klatki wideo w pikselach
 komunikacja_arduino(arduino, domyslne_x_glowy, domyslne_y_glowy, domyslne_x_oczu, domyslne_y_oczu, wiadomosc_startowa, wiadomosc_potwierdzajaca) #ustawienie glowy i oczu w domyslnej pozycji
 
 
@@ -58,16 +71,30 @@ while True:
         break
     szara_klatka = cv2.cvtColor(klatka, cv2.COLOR_BGR2GRAY) # Konwersja obrazu na skale szarosci, klatka - obraz RGB, szara_klatka - nowy obraz w odcienach szarosci
     twarz = face_cascade.detectMultiScale(szara_klatka, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30)) # Wykrywanie twarzy, skalowanie obrazu,
-
-
+    if len(twarz)>0: #obliczenie wykonywane są tylko i wylacznie gdy jakakolwiek twarz jest wykryta
+        x, y, sz_twarzy, wys_twarzy = twarz[0]
+        sektor=funkcje.sprawdz_sektor(x,sz_kamery,sz_twarzy,y, wys_kamery, wys_twarzy)
+        if sektor!=Sektor.SS:
+            aktualny_czas=time.perf_counter()
+        else:
+            poprzedni_czas=aktualny_czas
+        if (aktualny_czas-poprzedni_czas)>5:
+            #oś x
+            pozycja_x_glowy= ruch_glowy(sz_twarzy, sz_kamery, x, o_wspolczynnik_x, pozycja_x_glowy, minimum_x_glowy, maximum_x_glowy)
+            #oś y
+            pozycja_y_glowy= ruch_glowy(wys_twarzy,wys_kamery,y,g_wspolczynnik_y, pozycja_y_glowy, minimum_y_glowy, maximum_y_glowy)
+            #centrowanie oczu
+            pozycja_x_oczu=domyslne_x_oczu
+            pozycja_y_oczu=domyslne_y_oczu
+            #zresetowanie odliczania
+            poprzedni_czas=aktualny_czas
+        else:
+            pozycja_x_oczu=ruch_oczu(sz_twarzy, sz_kamery, x, o_wspolczynnik_x,minimum_x_oczu, maximum_x_oczu)
+            pozycja_y_oczu=ruch_oczu(wys_twarzy, wys_kamery, y, o_wspolczynnik_y, minimum_y_oczu, maximum_y_oczu)
+        komunikacja_arduino(arduino,pozycja_x_glowy, pozycja_y_glowy, pozycja_x_oczu, pozycja_y_oczu, wiadomosc_startowa, wiadomosc_potwierdzajaca)
+    cv2.imshow("nagrywanie", klatka)  # wyswietla klatke w okienku nagrywanie
+    if cv2.waitKey(1) & 0xFF == ord('q'):  # pozwolenie uzytkownikowi na zakonczenie dzialania programu poprzez nacisniecie klawisza 'q'
+        break
 nagranie.release()
 cv2.destroyAllWindows()
 arduino.close()
-
-#if odliczanie>=czas_glowa:
-            #odliczanie=0
-            #rusza glowa
-            #srodkowanie oczu
-            #time.sleep(2)
-
-        #podazanie oczami
